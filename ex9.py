a3196210a464e182e2c66d7e90330e7d1372f3a1
marianmna <solovevamaria242@gmail.com>
2024-03-01 20:55:59 +0700

    change2

---
+++ ex9.py
@@ -0,0 +1,16 @@
+from turtle import *
+import turtle
+
+t = turtle.Turtle()
+w = turtle.Screen()
+
+t.color('white', 'red')
+t.begin_fill()
+t.right(130)
+t.forward(150)
+t.circle(150, )
+t.goto(0, 0)
+t.end_fill()
+
+
+w.mainloop()
