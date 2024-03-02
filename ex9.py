b1f70fa67efc2c806c9da0c385bd5ac91e7d7d2d
marianmna <solovevamaria242@gmail.com>
2024-03-02 09:05:49 +0700

    change2

--- ex9.py
+++ ex9.py
@@ -4,13 +4,34 @@ import turtle
 t = turtle.Turtle()
 w = turtle.Screen()
 
-t.color('white', 'red')
 t.begin_fill()
-t.right(130)
-t.forward(150)
-t.circle(150, )
-t.goto(0, 0)
+t.fillcolor('blue')
+t.circle(100, 45)
 t.end_fill()
 
+t.begin_fill()
+t.fillcolor('brown')
+t.circle(100, 65)
+t.end_fill()
+
+t.begin_fill()
+t.fillcolor('green')
+t.circle(100, 90)
+t.end_fill()
+
+t.begin_fill()
+t.fillcolor('orange')
+t.circle(100, 45)
+t.end_fill()
+
+t.begin_fill()
+t.fillcolor('yellow')
+t.circle(100, 35)
+t.end_fill()
+
+t.begin_fill()
+t.fillcolor('red')
+t.circle(100, 80)
+t.end_fill()
 
 w.mainloop()
